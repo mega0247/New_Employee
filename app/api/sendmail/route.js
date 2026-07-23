55d38220f9b158a4cc42f8a6d596b4818ba1c573
mega0247 <lkwoplei0@gmail.com>
2026-07-23 05:06:08 -0500

    Update route.js

--- app/api/sendmail/route.js
+++ app/api/sendmail/route.js
@@ -343,7 +343,7 @@ console.log("SMTP connection verified successfully.");
                                 >
                                   <a
                                     style="color: #067ac1"
-                                    href="mailto:${data?.email}?subject=Re:%20${encodeURIComponent(subject)}"
+                                    href="mailto:${data?.email}"
                                     target="_blank"
                                     >${data?.email}</a
                                   >
